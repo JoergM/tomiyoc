7225b656c025c8ca37fa8fd18681475fc8c43a87
Joerg Mueller <joergm42.gmail.com>
2013-06-13 22:28:15 +0200

    Dynamic list of options, boostrap ...

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [ring/ring-jetty-adapter "1.1.8"]]
+                 [ring/ring-jetty-adapter "1.1.8"]
+                 [enlive "1.1.1"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler tomiyoc.handler/app}
   :main tomiyoc.handler
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
--- src/tomiyoc/handler.clj
+++ src/tomiyoc/handler.clj
@@ -1,12 +1,26 @@
 (ns tomiyoc.handler
   (:use [compojure.core]
-        [ring.adapter.jetty :only (run-jetty)])
+        [ring.adapter.jetty :only (run-jetty)]
+        [ring.util.response :only (resource-response)])
   (:require [compojure.handler :as handler]
-            [compojure.route :as route])
-  (:gen-class))
+            [compojure.route :as route]
+            [net.cgrand.enlive-html :as h])
+  (:gen-class ))
+
+(defn opt-snippet [o]
+  {:tag :div, :attrs {:class "span12"},
+   :content [{:tag :button, :attrs {:class "btn btn-danger btn-large btn-block"},
+              :content o}]})
+
+(h/deftemplate voting "templates/voting.html" [options]
+  [:.opt ] (h/clone-for [o options]
+             (h/content (opt-snippet o))))
+
+(defn voting-screen [request]
+  (voting ["Option 1" "Touch me if you can" "bLubberwasserprojekt" "Nummer 5 lebt"]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/voting" [] voting-screen)
   (route/resources "/")
   (route/not-found "Not Found"))
 
