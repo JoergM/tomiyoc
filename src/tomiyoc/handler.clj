c51444d37a26dd4da2ba8d9ef60555b292df1a0b
Joerg Mueller <joergm42.gmail.com>
2013-06-14 13:38:34 +0200

    auswertung will now contain real data.

--- src/tomiyoc/handler.clj
+++ src/tomiyoc/handler.clj
@@ -6,18 +6,27 @@
         [ring.adapter.jetty :only (run-jetty)]
         [ring.util.response :only (resource-response)])
   (:require [compojure.handler :as handler]
-            [compojure.route :as route])
+            [compojure.route :as route]
+            [clojure.data.json :as json])
   (:gen-class ))
 
 (defn save-vote-handler [option karte]
   (do (save-vote option karte)
     "SUCCESS"))
 
+(defn resultdata [request]
+  (let [values (get-summary)]
+    (json/write-str {:data (for [o (keys voting-options)] {:data [[o,(get values o)]] :bars {:show true :barWidth 0.5}})
+                     :ticks (for [k (keys voting-options)] [k (voting-options k)])
+                     :max (max (vals values))
+                     })))
+
 ;; Compojure Routes
 (defroutes app-routes
   (GET "/voting" [] voting-screen)
   (GET "/optionslist" [] options-as-json-response)
   (GET "/savevote" {{option :option karte :karte} :params} (save-vote-handler option karte))
+  (GET "/resultdata" [] resultdata)
   (route/resources "/")
   (route/not-found "Not Found"))
 
