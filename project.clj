c13b0c080a3406c876681ba7f96b38ba655ca9d5
Joerg Mueller <joergm42.gmail.com>
2013-06-14 10:26:12 +0200

    Weiterer Zwischenstand.

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [ring/ring-jetty-adapter "1.1.8"]
                  [enlive "1.1.1"]
                  [org.hsqldb/hsqldb "2.2.8"]
-                 [org.clojure/java.jdbc "0.3.0-alpha4"]]
+                 [org.clojure/java.jdbc "0.3.0-alpha4"]
+                 [org.clojure/data.json "0.2.2"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler tomiyoc.handler/app}
   :main tomiyoc.handler
--- src/tomiyoc/handler.clj
+++ src/tomiyoc/handler.clj
@@ -2,6 +2,7 @@
   (:use [compojure.core]
         [tomiyoc.view]
         [tomiyoc.database]
+        [tomiyoc.options]
         [ring.adapter.jetty :only (run-jetty)]
         [ring.util.response :only (resource-response)])
   (:require [compojure.handler :as handler]
@@ -11,6 +12,7 @@
 ;; Compojure Routes
 (defroutes app-routes
   (GET "/voting" [] voting-screen)
+  (GET "/optionslist" [] options-as-json-response)
   (route/resources "/")
   (route/not-found "Not Found"))
 
@@ -20,6 +22,6 @@
 ;; Damit wird gestartet
 (defn -main [& args]
   (do
-    (init-db optionsliste)
+    (init-db)
     (run-jetty #'app {:port 8080 :join? false})))
 
