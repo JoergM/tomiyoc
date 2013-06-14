a219c697993dbb2457696b6102c1704130e46831
oliver.otzen@hypoport.de <oliver.otzen@hypoport.de>
2013-06-14 14:34:30 +0200

    PUT instead of GET

--- src/tomiyoc/handler.clj
+++ src/tomiyoc/handler.clj
@@ -30,7 +30,7 @@
 (defroutes app-routes
   (GET "/voting" [] voting-screen)
   (GET "/optionslist" [] options-as-json-response)
-  (GET "/savevote" {{option :option karte :karte} :params} (save-vote-handler option karte))
+  (PUT "/savevote" {{option :option karte :karte} :params} (save-vote-handler option karte))
   (GET "/resultdata" [] resultdata)
   (GET "/summary" [] get-summary-wrapper)
   (route/resources "/")
